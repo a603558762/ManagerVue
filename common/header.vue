c0a9723110cd0909215e0c7f8c2589d968376763
a603558762 <603558762@qq.com>
2023-02-28 09:51:42 +0800

    first commit

---
+++ common/header.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+ import YButton from '/components/YButton'
+ import { mapMutations, mapState } from 'vuex'
+export default {
+  name: "header"
+}
+</script>
+
+<style scoped>
+
+</style>
